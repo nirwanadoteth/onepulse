122fe4d2d55db8c7f31e1cb4e6c33e284774d530
Irkham <121574317+nirwanadoteth@users.noreply.github.com>
2025-12-10 12:29:22 +0700

    feat(neynar): add Neynar API client and getScore function for bulk user score retrieval
    chore(package): update package.json to include @neynar/nodejs-sdk and swr dependencies

---
+++ lib/neynar.ts
@@ -0,0 +1,37 @@
+"use server";
+
+import {
+  Configuration,
+  isApiErrorResponse,
+  NeynarAPIClient,
+} from "@neynar/nodejs-sdk";
+
+type ScoreResponse = {
+  users: {
+    fid: number;
+    score: number;
+  }[];
+};
+
+const client = new NeynarAPIClient(
+  new Configuration({ apiKey: process.env.NEYNAR_API_KEY || "" })
+);
+
+export async function getScore(fids: number[]): Promise<ScoreResponse> {
+    try {
+    const response = await client.fetchBulkUsers({ fids });
+    return {
+      users: response.users.map((user) => ({
+        fid: user.fid || 0,
+        score: user.score || 0,
+      })),
+    };
+  } catch (error) {
+    if (isApiErrorResponse(error)) {
+      console.error("API Error:", error.response.data);
+    } else {
+      console.error("Error fetching user:", error);
+    }
+    return { users: [] };
+  }
+}
