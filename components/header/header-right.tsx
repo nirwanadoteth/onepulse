6eee65841f25dfafc7ee5eed40dd839f39e2a3f1
Irkham <121574317+nirwanadoteth@users.noreply.github.com>
2025-12-14 22:09:53 +0700

    refactor(header): remove unnecessary dropdown menu separator

--- components/header/header-right.tsx
+++ components/header/header-right.tsx
@@ -219,7 +219,6 @@ export const HeaderRight = memo(
                     </Toggle>
                   </DropdownMenuItem>
                 )}
-                <DropdownMenuSeparator />
               </>
             )}
 
