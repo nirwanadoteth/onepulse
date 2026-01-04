caf22cced10977ee4aaca69dc169206e92cf956e
Irkham <121574317+nirwanadoteth@users.noreply.github.com>
2026-01-04 22:52:02 +0700

    refactor(fonts): remove unused Inter font and simplify fontVariables export

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,8 +1,4 @@
-import {
-  Geist_Mono as FontMono,
-  Geist as FontSans,
-  Inter,
-} from "next/font/google";
+import { Geist_Mono as FontMono, Geist as FontSans } from "next/font/google";
 
 import { cn } from "@/lib/utils";
 
@@ -18,14 +14,4 @@ const fontMono = FontMono({
   display: "swap",
 });
 
-const fontInter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-  display: "swap",
-});
-
-export const fontVariables = cn(
-  fontSans.variable,
-  fontMono.variable,
-  fontInter.variable
-);
+export const fontVariables = cn(fontSans.variable, fontMono.variable);
