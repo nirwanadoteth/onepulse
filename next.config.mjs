b3446d2f5704f5b1a09b645215c1f5035b30a574
Irkham <121574317+nirwanadoteth@users.noreply.github.com>
2026-01-04 17:07:45 +0700

    feat(config): restructure nextConfig for improved logging and experimental features

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,10 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  compress: true,
-  experimental: {
-    inlineCss: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
   },
+  compress: true,
   images: {
     remotePatterns: [
       {
@@ -17,6 +17,50 @@ const nextConfig = {
   poweredByHeader: false,
   reactCompiler: true,
   reactStrictMode: true,
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  experimental: {
+    inlineCss: true,
+    optimizeCss: true,
+    optimizeServerReact: true,
+    optimizePackageImports: [
+      "lucide-react",
+      "@radix-ui/react-alert-dialog",
+      "@radix-ui/react-aspect-ratio",
+      "@radix-ui/react-avatar",
+      "@radix-ui/react-dialog",
+      "@radix-ui/react-dropdown-menu",
+      "@radix-ui/react-icons",
+      "@radix-ui/react-label",
+      "@radix-ui/react-popover",
+      "@radix-ui/react-progress",
+      "@radix-ui/react-scroll-area",
+      "@radix-ui/react-select",
+      "@radix-ui/react-separator",
+      "@radix-ui/react-slot",
+      "@radix-ui/react-tabs",
+      "@radix-ui/react-toggle",
+      "@radix-ui/react-tooltip",
+      "@coinbase/onchainkit",
+      "@farcaster/miniapp-sdk",
+      "@farcaster/miniapp-wagmi-connector",
+      "viem",
+      "viem/actions",
+      "viem/chains",
+      "viem/ens",
+      "viem/utils",
+      "wagmi",
+    ],
+  },
+  serverExternalPackages: ["@solana/web3.js", "rpc-websockets", "tr46"],
+  turbopack: {
+    resolveAlias: {
+      "wagmi/experimental": "wagmi",
+    },
+  },
 };
 
 export default nextConfig;
