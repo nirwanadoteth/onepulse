8d5882d4dee0ccf5bdc4c9081bb2f9d49ddfacaf
Irkham <121574317+nirwanadoteth@users.noreply.github.com>
2026-01-04 19:44:15 +0700

    refactor(client): remove superjson integration and clean up query client configuration

--- lib/client.ts
+++ lib/client.ts
@@ -1,24 +1,10 @@
-import {
-  defaultShouldDehydrateQuery,
-  isServer,
-  QueryClient,
-} from "@tanstack/react-query";
-import superjson from "superjson";
+import { isServer, QueryClient } from "@tanstack/react-query";
 
 function makeQueryClient() {
   return new QueryClient({
     defaultOptions: {
       queries: {
         staleTime: 30 * 1000,
-      },
-      dehydrate: {
-        serializeData: superjson.serialize,
-        shouldDehydrateQuery: (query) =>
-          defaultShouldDehydrateQuery(query) ||
-          query.state.status === "pending",
-      },
-      hydrate: {
-        deserializeData: superjson.deserialize,
       },
     },
   });
